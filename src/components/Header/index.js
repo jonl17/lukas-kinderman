ee195ae4babfaff7afb35e3af1c5aef290476b71
jonl17 <jglorange@gmail.com>
2020-08-09 20:50:19 +0000

    fonts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import "./styles.scss"
 const Header = ({ title }) => {
   return (
     <div className="d-flex align-items-center wrap py-4 headerWrap px-2 px-md-5">
-      <h1 className="m-0">{title}</h1>
+      <h1 className="m-0 bold">{title}</h1>
     </div>
   )
 }
