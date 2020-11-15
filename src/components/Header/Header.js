e1163e1073dfc0eb13a6d2cf12e77a8d9e060f90
jonl17 <jglorange@gmail.com>
2020-11-15 12:07:12 +0000

    header fix

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react"
+import { LayoutContext } from "../../layouts"
+import { Link } from "gatsby"
+
+const Header = () => {
+  const { headerContent } = useContext(LayoutContext)
+  return (
+    <div className="header d-flex justify-content-between w-100">
+      <div className="header__dynamic-content py-4 px-2 px-md-5">
+        {headerContent}
+      </div>
+      <Link
+        className="header__information-link d-flex align-items-center py-4 px-2 px-md-5"
+        to="/info"
+      >
+        <h1 className="bold m-0">Info</h1>
+      </Link>
+    </div>
+  )
+}
+
+export default Header
